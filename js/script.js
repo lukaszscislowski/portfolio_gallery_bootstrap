9df0810314147b6f908bb02ea40f52be16d109df
lukaszscislowski <lukas.scislowski@gmail.com>
2020-07-16 18:14:40 +0200

    newsletter js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //scroll to top function
 const arrow = document.querySelector('.arrow');
 let intervalId = 0; 
-// let pageHeight = window.height() /2;
+
 
 function scrollSet(){
     if(window.pageYOffset === 0) {
@@ -20,9 +20,74 @@ arrow.addEventListener('click', scrollTop);
 
 
 
-function showButton() {
-    if(pageYOffset > pageHeight) {
+// fade out
 
+function fadeOut(el){
+    el.style.opacity = 1;
+  
+    (function fade() {
+      if ((el.style.opacity -= .1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+  
+  // fade in
+  
+  function fadeIn(el, display){
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+  
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+  
+ function arrowSlide(){
+    const siteTop = document.body.scrollTop;
+    const pageHeight = document.body.scrollY / 2;
+    if (window.pageYOffset < pageHeight ){
+      fadeOut(arrow);
+    }else {
+      fadeIn(arrow); 
     }
-}
+ }
+  
+  window.addEventListener('scroll', arrowSlide);
 
+
+
+  // close form-popup
+
+  const close = document.querySelector('.popup-close')
+  const newsletter = document.querySelector('.popup-newsletter');
+  const btn = document.querySelector('.btn-block-footer');
+
+  function closePopup(e) {
+    e.preventDefault()
+    if(newsletter.style.display === "block") {
+      newsletter.style.display = "none";
+    }else {
+      newsletter.style.display = "block";
+    }
+  }
+
+  function openPopup(e) {
+    e.preventDefault()
+    if(newsletter.style.display === "none") {
+      newsletter.style.display = "block";
+    }else {
+      newsletter.style.display = "none";
+    }
+  }
+
+
+  close.addEventListener('click', closePopup);
+  btn.addEventListener('click', openPopup);
